69690362f0fe07cd13fc5a0adb6f80d67aa87d63
justinbellucci <justin.bellucci@me.com>
2020-06-02 10:48:15 -0600

    load checkpoint function

--- model_functions.py
+++ model_functions.py
@@ -2,6 +2,10 @@
 # DATE CREATED: 06_01_2020                                  
 # REVISED DATE: 
 
+import ast
+import torch
+from torchvision import models
+from classifier import Classifier
 
 ### ----------------------------------------------
 def load_class_dict(class_dict_path):
@@ -18,41 +22,9 @@ def load_class_dict(class_dict_path):
         
     return imagenet_class_dict
 
-### ----------------------------------------------
-def VGG16_predict(img_path):
-    """
-        Use pre-trained VGG-16 model to obtain index corresponding to 
-        predicted ImageNet class for image at specified path
-    
-        Args:
-            img_path: path to an image
-        
-        Returns:
-            Index corresponding to VGG-16 model's prediction
-    """
-    # move model to correct device
-    device = torch.device("cpu")
-    VGG16.to(device)
-    
-    with torch.no_grad():
-        # process image
-        image = process_image(img_path)
-        image = image.unsqueeze_(0)
-        image.to(device)
-        
-        output = VGG16(image)
-        
-        # convert output probabilities to predicted class
-        _, class_tensor = torch.max(output, 1)
-
-        # convert output tensor to numpy array
-        class_pred = class_tensor.numpy()
-        
-    return  class_pred
-
 
 ### ----------------------------------------------
-def load_checkpoint(filepath):
+def load_checkpoint(filepath='model_transfer.pt'):
     """Load a model checkpoint and rebuild model architecture
     
        Arguments:
@@ -67,8 +39,7 @@ def load_checkpoint(filepath):
     model.classifier = Classifier()
 #     checkpoint = torch.load(filepath, map_location=("cuda" if torch.cuda.is_available() else "cpu"))
     checkpoint = torch.load(filepath)
-    model.load_state_dict(checkpoint['state_dict'])
-    model.class_to_idx = checkpoint['class_to_idx']
+    model.load_state_dict(checkpoint)
     
     # Freeze the parameters so we dont backpropagate through them
     for param in model.parameters():
@@ -76,4 +47,3 @@ def load_checkpoint(filepath):
         
     return model
 ### ----------------------------------------------
-detector_type = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
