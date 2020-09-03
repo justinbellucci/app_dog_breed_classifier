6b0147924474ed46314ae092ec0fd49eb585ebc2
justinbellucci <justin.bellucci@me.com>
2020-09-03 17:43:42 -0600

    playing around with streamlit

---
+++ main_app.py
@@ -0,0 +1,20 @@
+# PROGRAMMER: Justin Bellucci 
+# DATE CREATED: 09_03_2020                                  
+# REVISED DATE: 
+
+# Import python modules
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title('Dog Breed Classifier App')
+img = st.file_uploader('Upload Image', type='jpg')
+st.text('{}'.format(type(img)))
+placeholder = st.empty()
+
+if not st.button('Reset'):
+    st.image(img, output_format='JPEG', use_column_width=True)
+else:
+    st.image() 
