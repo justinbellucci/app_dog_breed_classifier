8d96d71efe568c9387644a4ee5de516eedb2ee54
justinbellucci <justin.bellucci@me.com>
2021-12-14 13:03:12 -0700

    AJAX image processing functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import io
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, url_for
 
 import backend.detectors as detectors
 from backend.predict import predict_breed
@@ -9,32 +9,67 @@ from backend.model_functions import load_class_names
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url) # redirect to same page
-        file = request.files['file'] # get file from request
-        if not file:
-            return # add error message
+    # loading icon to wait for the image to be loaded
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file in the request'})
+        return resp
+
+    file = request.files['file']
+
+    # process image
+    img_bytes = file.read()
+    # run image throuh model
+    is_dog = detectors.dog_detector(io.BytesIO(img_bytes))
+    probs, classes = predict_breed(io.BytesIO(img_bytes))
+    class_names = load_class_names()
+
+    # add model output to JSON files
+    dog_detected = {"is_dog": is_dog}
+    dog_breeds = [{"breed": class_names[classes[0]], "prob": probs[0]},
+                    {"breed": class_names[classes[1]], "prob": probs[1]},
+                    {"breed": class_names[classes[2]], "prob": probs[2]},
+                    {"breed": class_names[classes[3]], "prob": probs[3]},
+                    {"breed": class_names[classes[4]], "prob": probs[4]}]
+
+    if is_dog:
+        resp = jsonify({'message' : 'The image is a dog'})
+    else:
+        resp = jsonify({'message' : 'The image is not a dog'})
+
+    return resp
+
+# @app.route('/', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             return redirect(request.url) # redirect to same page
+#         file = request.files['file'] # get file from request
+#         if not file:
+#             return # add error message
     
-        # process image
-        img_bytes = file.read()
-        # run image throuh model
-        is_dog = detectors.dog_detector(io.BytesIO(img_bytes))
-        probs, classes = predict_breed(io.BytesIO(img_bytes))
-        class_names = load_class_names()
+#         # process image
+#         img_bytes = file.read()
+#         # run image throuh model
+#         is_dog = detectors.dog_detector(io.BytesIO(img_bytes))
+#         probs, classes = predict_breed(io.BytesIO(img_bytes))
+#         class_names = load_class_names()
 
-        # add model output to JSON files
-        dog_detected = {"is_dog": is_dog}
-        dog_breeds = [{"breed": class_names[classes[0]], "prob": probs[0]},
-                      {"breed": class_names[classes[1]], "prob": probs[1]},
-                      {"breed": class_names[classes[2]], "prob": probs[2]},
-                      {"breed": class_names[classes[3]], "prob": probs[3]},
-                      {"breed": class_names[classes[4]], "prob": probs[4]}]
+#         # add model output to JSON files
+#         dog_detected = {"is_dog": is_dog}
+#         dog_breeds = [{"breed": class_names[classes[0]], "prob": probs[0]},
+#                       {"breed": class_names[classes[1]], "prob": probs[1]},
+#                       {"breed": class_names[classes[2]], "prob": probs[2]},
+#                       {"breed": class_names[classes[3]], "prob": probs[3]},
+#                       {"breed": class_names[classes[4]], "prob": probs[4]}]
 
-        return render_template('results.html', title='Results', dog_detected=dog_detected, dog_breeds=dog_breeds)
-    return render_template('index.html', title='Index')
+#         return render_template('results.html', title='Results', dog_detected=dog_detected, dog_breeds=dog_breeds)
+#     return render_template('index.html', title='Index')
 
 if __name__ == "__main__":
     app.run(debug=True)
